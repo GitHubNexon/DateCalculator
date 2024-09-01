ac1a46c43dc4fa2f40263f381494bf9c0ddb7f80
GitHubNexon <johnmarkpulmano052802@gmail.com>
2024-09-01 17:38:46 +0800

    add bg in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import "../styles/custom.css"
 
 const Countdown = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-auto text-gray-800">
+    <div className="container flex flex-col items-center justify-center h-screen text-white text-center">
       <h1 className="text-4xl font-bold mb-4">Exciting Countdown Coming Soon!</h1>
       <p className="text-lg text-center px-4 md:px-8">
         Stay tuned for our upcoming event. We’re working hard to bring you an amazing experience!
